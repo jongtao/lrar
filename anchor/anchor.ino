c1789e9ba76571b797ef689eeb3a0a89bc68b08a
J. Tao <jon.g.tao@gmail.com>
2016-05-08 05:18:07 -0700

    tried lowered delay, deleted unused stuff

--- anchor/anchor.ino
+++ anchor/anchor.ino
@@ -3,24 +3,25 @@
 
 
 #define MY_ID 0x0
+#define DELAY_MS 20
+#define TIMEOUT_US 3000 
 
 int ss = 10;
 int rstpin = 9;
 long temp;
 
 DW1000 dw = DW1000(ss, rstpin);
-//volatile bool SPIevent = false;
-//bool is_sent = false;
-//bool is_receive = false;
 uint8_t iter=0;
 unsigned long now_time = 0;
-//unsigned long last_time = 0;
 uint64_t range1_time =0;
 uint64_t reply1_time =0;
 uint64_t delta_time = 0;
 byte tx_buffer[]={0x1,MY_ID, iter};
 
 uint8_t timeout = 0;
+#define NUM_TAGS 3
+uint8_t tag_numbers[NUM_TAGS] = {0x01, 0x02, 0x03};
+uint8_t tag = 0; // start with tag 0
 
 
 uint32_t range_buffer[]={0,0,0,0,0}; // range to report
@@ -52,38 +53,9 @@ typedef enum MY_STATUS{
 
 MY_STATUS_t my_status=INIT;
 
-/*
-void isr_handler(){
- // Serial.println("isr");
-  SPIevent = true;
-}
-(
-*/
-
-/*
-void event_handler(){
-  uint16_t event_code = dw.readReg(0x0F, 0, 0, 2);
-  Serial.println(event_code, BIN);
-  if (bitRead(event_code, 7)){
-    is_sent = true;
-  }
-  if (bitRead(event_code, 13)){
-    is_receive=true;
-  }
-}
-*/
-
 
 
 void clearReceive(){
- /*
-  dw.clearSystemStatus(SYS_MASK_MRXPRD);
-  dw.clearSystemStatus(SYS_MASK_MRXSFDD);
-  dw.clearSystemStatus(SYS_MASK_MLDEDONE);
-  dw.clearSystemStatus(SYS_MASK_MRXPHD);
-  dw.clearSystemStatus(SYS_MASK_MRXDFR);
-  dw.clearSystemStatus(SYS_MASK_MRXDFR);
-  */
   dw.writeReg(0x0F, 0,0, (uint32_t)(0x3F<<11), 4);
 }
 
@@ -99,53 +71,15 @@ void newreceive(){
   dw.writeReg(0x0d,0,0, (uint32_t)(0x1<<6), 4);
 }
 
-/*
-void sentFixMsg(){
-  byte tx_buffer[]={0x1,0x2,iter};
-  dw.writeTxBuffer(tx_buffer, 3);
-  dw.startTx();
-  delay(10);
-  iter++;
-}
-*/
 
-/*
-void sentBlink(){
-  byte tx_buffer[]={0x1,iter};
-  dw.writeTxBuffer(tx_buffer, 2);
-  dw.startTx();
-  delay(10);
-  iter++;
-}
-*/
+void sentRange1(uint8_t target_ID){
 
-void sentRange1(){
-
+	tx_buffer[1] = target_ID;
   dw.writeTxBuffer(tx_buffer, 3);
   dw.startTx();
   iter++; 
   // setup delay
 }
-
-/*
-
-void softReset(){
-  //reset in 5 seconds inactivity
-    Serial.println("timeout. Soft Reset");
-    my_status = INIT;
-    now_time = micros();
-    last_time = micros();
-    clearSent();
-    clearReceive();
-}
-*/
-
-/*
-void newDelaysent(){
-  dw.writeReg(0x0d,0,0, (uint16_t)(0x40), 2);
-  dw.writeReg(0x0d,0,0, (uint16_t)(0x02), 2);
-}
-*/
 
 
 void setup() {
@@ -214,11 +148,11 @@ inline void DW1000_reset(void)
 
 
 
-inline void DW1000_send(void)
+inline void DW1000_send(uint8_t target_ID)
 {
 	//Serial.println("RANGE1");
 	newsent();
-	sentRange1();
+	sentRange1(target_ID);
 	while(!issent())
 		;
 	// record range1_time
@@ -238,7 +172,7 @@ inline void DW1000_receive(void)
 	now_time = micros();
 	while(!isreceive())
 	{
-		if (micros()-now_time>1000000)
+		if (micros()-now_time > TIMEOUT_US)
 		{
 			timeout = 1;
 			DW1000_reset(); // reset and restart on timeout
@@ -249,9 +183,11 @@ inline void DW1000_receive(void)
 	clearReceive();
 	reply1_time = getTimestamp(RX_TIME_ID);
 	delta_time = reply1_time - range1_time;
-	delta_time = delta_time*50/3/100-20000000;
-	delta_time = delta_time-1310000;
+	delta_time = (delta_time-65000000)*50/3/100;
+	delta_time = delta_time-11000;
 	push_buffer(range_buffer, (uint32_t)delta_time);
+	Serial.print("Tag: ");
+	Serial.println(tag_numbers[tag]);
 	Serial.print("delta time is: ");
 	Serial.println(avg_buffer(range_buffer));
 } // DW1000_receive()
@@ -265,7 +201,7 @@ inline void post_range(void)
 	//Serial.println(reply1_time-range1_time-31949);
 	now_time = micros();
 	my_status = INIT;
-	delay(1000);
+	delay(DELAY_MS);
 }
 
 
@@ -274,9 +210,14 @@ inline void post_range(void)
 void loop()
 {
 	DW1000_reset();
-	DW1000_send();
+	DW1000_send(tag_numbers[tag]);
 	DW1000_receive();
+
+	tag++;
+	if(tag >= NUM_TAGS)
+		tag = 0;
 
 	if(!timeout)
 		post_range();
 } // loop()
+
